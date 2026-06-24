cac7ebe310d7a9cf1fc56a1c515224597e7dff99
navyakulshreshtha33-a11y <navyakulshreshtha33@gmail.com>
2026-06-24 19:58:26 +0530

    day 3 ques1 done

--- Week_4/Day_/Search_in_a_binary_search_tree.java
+++ Week_4/Day_/Search_in_a_binary_search_tree.java
@@ -18,7 +18,7 @@
     public TreeNode searchBST(TreeNode root, int val) {
 
         while (root != null) {
-            if (root.val == val) {
+            if (root.val==val) {
                 return root;
             }
 
