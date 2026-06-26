2eece1dd39cffe4a52ac8d8c46075c13eaca9483
navyakulshreshtha33-a11y <navyakulshreshtha33@gmail.com>
2026-06-26 18:02:55 +0530

    day5 ques 2 added

---
+++ Week_4/Day_5/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+class Solution {
+
+    int preIndex = 0;
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        // Store inorder value -> index
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return build(preorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] preorder, int left, int right) {
+
+        if (left > right)
+            return null;
+
+        // Current root
+        int rootValue = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootValue);
+
+        // Find root index in inorder
+        int mid = map.get(rootValue);
+
+        // Build left subtree
+        root.left = build(preorder, left, mid - 1);
+
+        // Build right subtree
+        root.right = build(preorder, mid + 1, right);
+
+        return root;
+    }
+}
